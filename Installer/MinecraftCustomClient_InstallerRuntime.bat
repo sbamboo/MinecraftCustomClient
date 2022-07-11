bb76a9b3e70089e9422aff672442993d9b3f2c0b
simonkalmiclaesson <simon.claesson8@gmail.com>
2022-07-11 02:34:48 +0200

    Update MinecraftCustomClient_InstallerRuntime.bat

--- Installer/MinecraftCustomClient_InstallerRuntime.bat
+++ Installer/MinecraftCustomClient_InstallerRuntime.bat
@@ -1,27 +1,10 @@
 @:: Powershell Installer / Runtime, Runns a script and installs powershell if needed. Created by Simon Kalmi Claesson. License/Info: https://sites.google.com/view/scofficial/projects/powershell-installerruntime    Version: 2.3
 @::
-@powershell "$host.ui.rawui.windowtitle = 'Pwsh Runtime v.2.3'; if((Get-Command -Name Pwsh -ErrorAction Ignore)) {}else{write-host "Installing powershell..."; $script = invoke-restmethod https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.ps1; $script > tmp.ps1; powershell -ep bypass .\tmp.ps1 "-addtopath"; del tmp.ps1}" && start pwsh -c "Get-Content %0 | Select -Skip 12 | Out-File pwshRuntime_Tempscript.ps1; .\pwshRuntime_Tempscript.ps1; del pwshRuntime_Tempscript.ps1"
+@powershell "$host.ui.rawui.windowtitle = 'Pwsh Runtime v.2.3'; if((Get-Command -Name Pwsh -ErrorAction Ignore)) {}else{write-host "Installing powershell..."; $script = invoke-restmethod https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.ps1; $script > tmp.ps1; powershell -ep bypass .\tmp.ps1 "-addtopath"; del tmp.ps1}" && start pwsh -c "$adress = 'https://raw.githubusercontent.com/simonkalmiclaesson/MinecraftCustomClient/main/Installer/MinecraftCustomClient_Installer.ps1'; $name = $adress | Split-Path -leaf; if (test-path $name) {. .\$name} else {curl -s $adress | Out-File -file $name; . .\$name}"
 @goto :eof
 #!/bin/sh
 clear
 echo "Pwsh Runtime v.2.3"
 echo "--------------------------------------------------------"
 echo ""
-hash pwsh 2>/dev/null || { echo >&2 ""; bash <(curl -s https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh); }; clear; pwsh -c "Get-Content $0 | Select -Skip 12 | Out-File pwshRuntime_Tempscript.ps1; .\pwshRuntime_Tempscript.ps1; del pwshRuntime_Tempscript.ps1"
-exit
-
-#Bundled Powershell script
-
-#Varibles
-$app_adress = "https://raw.githubusercontent.com/simonkalmiclaesson/MinecraftCustomClient/main/Installer/MinecraftCustomClient_Installer.ps1"
-$app_name = $app_adress | split-path -leaf
-
-#Check for vdat file
-if (Test-path $app_name) {
-    start pwsh "-file $app_name"
-    exit
-} else {
-    curl -s "$app_adress" | Out-File -file "$app_name"
-    start pwsh "-file $app_name"
-    exit
-}
+hash pwsh 2>/dev/null || { echo >&2 ""; bash <(curl -s https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh); }; clear; pwsh -c "$adress = 'https://raw.githubusercontent.com/simonkalmiclaesson/MinecraftCustomClient/main/Installer/MinecraftCustomClient_Installer.ps1'; $name = $adress | Split-Path -leaf; if (test-path $name) {. .\$name} else {curl -s $adress | Out-File -file $name; . .\$name}"
