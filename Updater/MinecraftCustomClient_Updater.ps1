5907112f921eacf764b6d19c46c778edd6782068
StalkedBamboo <simon.claesson8@gmail.com>
2022-07-26 15:01:19 +0200

    Create MinecraftCustomClient_Updater.ps1

---
+++ Updater/MinecraftCustomClient_Updater.ps1
@@ -0,0 +1,19 @@
+#MinecraftCustomClient_Updater 1.1
+#vars
+$host.ui.rawui.windowtitle = "Updating..."
+$adress = "https://raw.githubusercontent.com/simonkalmiclaesson/MinecraftCustomClient/main/Installer/MinecraftCustomClient.bat"
+$name = $adress | split-path -leaf
+
+#Check in parent dir
+cd ..
+if (test-path "$name") {} else {
+  write-host "Updater could not find a installer in the same directory, please check." -f red
+  pause
+  exit
+}
+
+#fix
+del "$name"
+curl -s "$adress" | Out-File -file $name
+start $name -argument "-HasUpdated"
+exit
